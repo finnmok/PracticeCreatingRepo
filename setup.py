dfe200907320bd17930150646c1553964b8d50dd
Finn <107573572+finnmok@users.noreply.github.com>
2024-07-02 10:39:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,8 @@
+from distutils.core import setup
 from setuptools import tools
+
+setup(name='myexamplepkg',
+      version='1.0',
+      
+      # list folders, not files
+      packages=['myexamplepkg'] # Include packages in the project)
